f6abe4b4de542e01bcd177f9f515e323474422a9
eswar-7116 <eswardudi06@gmail.com>
2026-05-02 21:09:44 +0530

    add sample test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
+}
