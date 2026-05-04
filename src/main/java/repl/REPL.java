c4b46df6a9314ff5f36f360b490dc12776b91974
eswar-7116 <eswardudi06@gmail.com>
2026-05-04 23:46:20 +0530

    implement REPL

---
+++ src/main/java/repl/REPL.java
@@ -0,0 +1,39 @@
+package repl;
+
+import interpreter.Interpreter;
+import lexer.Lexer;
+import parser.Parser;
+import stmt.Stmt;
+import token.Token;
+
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class REPL {
+    static void main() throws Exception {
+        Lexer lexer = new Lexer(new InputStreamReader(System.in));
+        Map<String, Double> symbolTable = new HashMap<>();
+        Interpreter interpreter = new Interpreter(symbolTable);
+
+        while (true) {
+            System.out.print(">>> ");
+
+            List<Token> tokens = lexer.lexLine();
+            if (tokens == null) break; // EOF
+
+            if (tokens.isEmpty()) continue;
+
+            try {
+                Parser parser = new Parser(tokens);
+                Stmt stmt = parser.parse();
+
+                interpreter.execute(stmt);
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+}
